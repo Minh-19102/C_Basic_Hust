0da916b576a7f521d041a0a927e1c8407006ac38
minhdn-es76d <minhdn.es76d@gmail.com>
2021-12-12 23:16:21 +0700

    bs2

---
+++ source_code/week5/ListQueue.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int ElementType;
+typedef struct node {
+   ElementType Element;
+   struct node *Next;
+} Node;
+typedef Node *Position;
+typedef struct {
+   Position Front, Rear;
+} Queue;
+void MakeNullQueue(Queue *Q) {
+   Position Header;
+   Header = (Node *)malloc(sizeof(Node));
+   Header->Next = NULL;
+   Q->Front = Header;
+   Q->Rear = Header;
+}
+int EmptyQueue(Queue Q) { return (Q.Front == Q.Rear); }
+void EnQueue(ElementType X, Queue *Q) {
+   Q->Rear->Next = (Node *)malloc(sizeof(Node));
+   Q->Rear = Q->Rear->Next;
+   Q->Rear->Element = X;
+   Q->Rear->Next = NULL;
+}
+void DeQueue(Queue *Q) {
+   if (!EmptyQueue(*Q)) {
+      Position T;
+      T = Q->Front;
+      Q->Front = Q->Front->Next;
+      free(T);
+   } else
+      printf("Error : Queue is empty.");
+}
+int main() {}
